01e70658345870d55200560f8ee2d007b15fbd71
yangzhijie <yangzhijie@vimtag.it>
2025-06-18 11:56:17 +0800

    add Vundle.vim

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -7,6 +7,8 @@ source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
+apt-get install ctags
+
 try
 source ~/.vim_runtime/my_configs.vim
 catch
